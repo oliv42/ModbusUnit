488acd6a67a8b53d262cf9f5414872228062763b
Reboul <olivier.reboul@gmail.com>
2025-07-12 20:13:40 +0200

    boilerplate0.1

--- modBusUnit/src/electron/main.ts
+++ modBusUnit/src/electron/main.ts
@@ -1,9 +1,14 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 import { isDev } from "./utils.js";
+import { getPreloadPath } from "./pathResolver.js";
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+  });
   if (isDev()) {
     mainWindow.loadURL("http://localhost:5173");
   } else {
